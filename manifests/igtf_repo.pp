09335ce9c73e95dea8abe4ffbc1323cebc8d96ea
Pablo Orviz <orviz@ifca.unican.es>
2017-04-26 17:30:54 +0200

    add APT key for IGTF

--- manifests/igtf_repo.pp
+++ manifests/igtf_repo.pp
@@ -10,10 +10,17 @@ class umd::igtf_repo::yum {
 }
 
 class umd::igtf_repo::apt {
+    apt::key {
+        "EUGridPMA":
+            source => "https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3", 
+            id     => "D12E922822BE64D50146188BC32D99C83CDBBC71",
+    }
+
     apt::source {
         "egi-igtf":
             location => "http://repository.egi.eu/sw/production/cas/1/current",
             release  => "egi-igtf",
             repos    => "core",
+            require  => Apt::Key["EUGridPMA"]
     }
 }
