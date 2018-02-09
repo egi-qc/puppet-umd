dbf2ad40d02995a1a1fd7dc090547372376b1cb8
Pablo Orviz <orviz@ifca.unican.es>
2018-02-09 11:20:42 +0000

    Fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -124,13 +124,21 @@ class umd::distro::umd {
             }
         }
         elsif $::operatingsystem in ["Scientific", "CentOS"]  and $::operatingsystemmajrelease == "6" {
-            if $umd::params::release == "4" {
-                $pkg = "${umd::params::release_map[4][sl6]}"
+            if $::operatingsystem == "Scientific" {
+                if $umd::params::release == "4" {
+                    $pkg = "${umd::params::release_map[4][sl6]}"
+                }
+                elsif $umd::params::release == "3" {
+                    $pkg = "${umd::params::release_map[3][sl6]}"
+                }
+	    elsif $::operatingsystem == "CentOS" {
+                if $umd::params::release == "4" {
+                    $pkg = "${umd::params::release_map[4][centos6]}"
+                }
+                elsif $umd::params::release == "3" {
+                    $pkg = "${umd::params::release_map[3][centos6]}"
+                }
             }
-            elsif $umd::params::release == "3" {
-                $pkg = "${umd::params::release_map[3][sl6]}"
-            }
-
             package {
                 "umd-release":
                     provider => "rpm",
